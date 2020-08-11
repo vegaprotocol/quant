336539afe930997003f3d8fd0deb4a2af8014265
Witold <witold@vega.xyz>
2020-08-11 17:19:26 +0200

    Add tests for lognormal distribution

--- riskmodelbs/distribution_test.go
+++ riskmodelbs/distribution_test.go
@@ -1,6 +1,7 @@
 package riskmodelbs
 
 import (
+	"math"
 	"testing"
 
 	"code.vegaprotocol.io/quant/interfaces"
@@ -11,12 +12,33 @@ func TestBSIsAnalyticalModel(t *testing.T) {
 	const mu float64 = 0.0
 	const sigma float64 = 1.5
 
-	const tau = 1.0 / 365 / 24 / 60
-	const lambda float64 = 0.01
-
 	var analyticBs interfaces.AnalyticalModel = ModelParamsBS{Mu: 0, R: 0, Sigma: 1.5}
 
 	if analyticBs == nil {
 		t.Error("Expeced ModelParamsBS to implement AnalyticalModel interface")
+	}
+}
+
+func TestLogNormalDistributionInternallyConsistent(t *testing.T) {
+	const r float64 = 0.0
+	const mu float64 = 0.2
+	const sigma float64 = 1.5
+	const S0 float64 = 123.0
+	const tau = 1
+
+	bsModelParameters := ModelParamsBS{Mu: 0, R: 0, Sigma: 1.5}
+
+	distribution := bsModelParameters.GetProbabilityDistribution(S0, tau)
+
+	ps := []float64{0, 0.001, 0.2, 0.5, 1, 10, 20, 30, 100, 1000, 10000}
+
+	tolerance := bsModelParameters.GetProbabilityTolerance()
+	for _, p := range ps {
+		cdf := distribution.CDF(p)
+		pRecalculated := distribution.Quantile(cdf)
+
+		if math.Abs(p-pRecalculated) > tolerance {
+			t.Errorf("Error=%g is more than tolerance (%g)", math.Abs(p-pRecalculated), tolerance)
+		}
 	}
 }
