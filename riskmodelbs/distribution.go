bf26f70933874de8e18be4dbee657dc845fc114d
Witold <witold@vega.xyz>
2020-09-08 13:27:54 +0200

    Add comments

--- riskmodelbs/distribution.go
+++ riskmodelbs/distribution.go
@@ -10,6 +10,7 @@ import (
 
 const probabilityTolerance = 1e-3
 
+// GetProbabilityDistribution returns the log normal distribution correcposning to the suplied model parameters, the current stock price S and time horizon tau.
 func (modelParams ModelParamsBS) GetProbabilityDistribution(S, tau float64) interfaces.AnalyticalDistribution {
 	// See: http://www-2.rotman.utoronto.ca/~hull/TechnicalNotes/TechnicalNote2.pdf
 	m := math.Log(S) + (modelParams.Mu-0.5*modelParams.Sigma*modelParams.Sigma)*tau
@@ -18,6 +19,8 @@ func (modelParams ModelParamsBS) GetProbabilityDistribution(S, tau float64) inte
 	return &distuv.LogNormal{Mu: m, Sigma: stdDev}
 }
 
-func (modelParams ModelParamsBS) GetProbabilityTolerance() float64 {
-	return probabilityTolerance
+// GetProbabilityTolerance specifies the probability tolerance alphaModel that the model supports. It shouldn't be used for any calculations involving alpha < alphaModel or alpha > 1-alphaModel
+func (modelParams ModelParamsBS) GetProbabilityTolerance() (alphaModel float64) {
+	alphaModel = probabilityTolerance
+	return alphaModel
 }
